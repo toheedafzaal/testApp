0ebafdab35fd1315afcc7e456d6b4ff5f0d4ab2e
toheed afzaal <toheedafzaal620@gmail.com>
2022-06-15 17:57:51 +0500

    v 7 is pushed

--- testApplication/Form1.Designer.cs
+++ testApplication/Form1.Designer.cs
@@ -29,17 +29,20 @@ namespace testApplication
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(326, 126);
+            this.label1.Location = new System.Drawing.Point(325, 112);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(51, 13);
             this.label1.TabIndex = 0;
@@ -48,51 +51,74 @@ namespace testApplication
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(326, 166);
+            this.label2.Location = new System.Drawing.Point(325, 153);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.Size = new System.Drawing.Size(51, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Version 2 is released";
+            this.label2.Text = "Version 2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(326, 217);
+            this.label3.Location = new System.Drawing.Point(325, 185);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.Size = new System.Drawing.Size(51, 13);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Version 3 is released";
+            this.label3.Text = "Version 3";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(326, 252);
+            this.label4.Location = new System.Drawing.Point(325, 220);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.Size = new System.Drawing.Size(51, 13);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Version 4 is released";
+            this.label4.Text = "Version 4";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(326, 286);
+            this.label5.Location = new System.Drawing.Point(325, 256);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(104, 13);
+            this.label5.Size = new System.Drawing.Size(51, 13);
             this.label5.TabIndex = 4;
-            this.label5.Text = "Version 5 is released";
+            this.label5.Text = "Version 5";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(325, 293);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Version 6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(325, 323);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Version 7";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "VimeoVSV";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,6 +131,8 @@ namespace testApplication
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- testApplication/Form1.cs
+++ testApplication/Form1.cs
@@ -1,20 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace testApplication
 {
+    [ComVisibleAttribute(true)]
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //getVideoIds();
+            //this.webBrowser1.ObjectForScripting = this;
+            //Assembly assembly = Assembly.GetExecutingAssembly();
+            //StreamReader reader = new StreamReader(assembly.GetManifestResourceStream("testApplication.HTML.htm"));
+            //webBrowser1.DocumentText = reader.ReadToEnd();
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+        //public string getVideoIds()
+        //{
+        //    //var constring = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=dbVimeo;User ID=sa;Password=sa";
+        //    var constring = ConfigurationManager.ConnectionStrings["vimeocs"].ConnectionString;
+        //    SqlConnection con = new SqlConnection(constring);
+        //    SqlCommand selectvideoids = new SqlCommand("Select VideoId from tblVideos", con);
+        //    DataTable dt = new DataTable();
+        //    con.Open();
+        //    SqlDataReader selectedVids = selectvideoids.ExecuteReader();
+        //    dt.Load(selectedVids);
+        //    con.Close();
+        //    var ids = new List<int>();
+        //    //var ids = new object();
+        //    foreach (DataRow row in dt.Rows)
+        //    {
+        //        ids.Add(Convert.ToInt32(row["VideoId"]));
+        //    }
+        //    List<string> videoids = ids.ConvertAll<string>(x => x.ToString());
+        //    //String.Join(", ", videoids);
+        //    var idssss = String.Join(", ", videoids);
+        //    //Console.WriteLine(String.Join(", ", videoids));
+        //    //var videoids = dt.Rows;
+        //    //string[] videoids = new string[] { "590305554", "36319428", "95212995", "67423133", "131484417" };
+        //    //int[] videoids = { 590305554, 36319428, 95212995, 67423133, 131484417 };
+        //    //string test = "1323";
+        //    return idssss;
+        //}
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
